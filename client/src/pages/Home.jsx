0e130d47351a90d7e446d36945a4a8c2ad1a27bf
MuhammadFaizan144 <fg7829098@gmail.com>
2026-07-28 12:33:14 +0500

    start working on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <section>
+        <div className="">
+          <div className="">
+            <img src="/images/model01.png" alt="" />
+          </div>
+          <div className="">
+            <h1>The Minecraft MMO Survival</h1>
+            <p>A server where you can experience MMO style RPG leveling and fully customized survival world</p>
+            <div className="">
+              <div className=""></div>
+              <div className=""></div>
+            </div>
+            <div className="">
+              
+            </div>
+          </div>
+        </div>
+      </section>
+      <article></article>
+    </section>
   )
 }
 
